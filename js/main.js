e960250deeee1c8bd7b39fb9d33f90c1b82ad244
Evgeny Babkin <babkinevgeny@gmail.com>
2018-03-28 11:08:59 +0300

    Added slider recomend

--- js/main.js
+++ js/main.js
@@ -290,3 +290,52 @@ $(document).ready(function() {
 	});
 
 });
+
+/*Owl carousel*/
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        650:{
+            items:3,
+            nav:true
+        }
+    },
+    dots: false,
+    center: true,
+    autoplay: true
+})
+});
+
+/*Magnific popup*/
+$(document).ready(function() {
+  $('.item-link').magnificPopup({
+    type:'image',
+    gallery: {
+      enabled: true, // set to true to enable gallery
+
+      preload: [0,2], // read about this option in next Lazy-loading section
+
+        navigateByImgClick: true,
+
+  arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+    tPrev: 'Previous (Left arrow key)', // title for left button
+    tNext: 'Next (Right arrow key)', // title for right button
+    tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+  },
+  removalDelay: 300,
+
+  // Class that is added to popup wrapper and background
+  // make it unique to apply your CSS animations just to this exact popup
+  mainClass: 'mfp-fade'
+
+
+  });
+});
