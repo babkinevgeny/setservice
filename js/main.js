92c6136370f5559b70c2c771551754df20662771
Evgeny Babkin <babkinevgeny@gmail.com>
2018-03-28 10:31:21 +0300

    Added about section

--- js/main.js
+++ js/main.js
@@ -114,8 +114,14 @@ $('.btn-nav-close').click(function() {
 });
 
 $(document).on('click touchstart', '.mouse', function(){
-			var hei = $(document).find('.page-header').outerHeight();
-			$('html, body').animate({ scrollTop: hei-81}, 500);
+      var hei = $(document).find('.page-header').outerHeight();
+      if($(window).height() >= 992) {
+        console.log("я длиный экран");
+        $('html, body').animate({ scrollTop: hei+37}, 500);
+      } else {
+        console.log("я короткий экран");
+        $('html, body').animate({ scrollTop: hei-81}, 500);
+      }
 });
 
 window.onscroll = function() {
@@ -144,13 +150,143 @@ window.onscroll = function() {
 		});
 }
 
-while(true) {
-  $(document).find('.mouse .mouse-dot').stop()
-    .css({ top : 10, left : 11, opacity : 1 })
-    .delay(200)
-    .animate({ top : 29, opacity : 0 },1500,'swing',function(){
-  $(this).css({ top : 10 })
-  .animate({ opacity : 1 },500,'swing',function(){ aniamate(); });
+// while(true) {
+//   $(document).find('.mouse .mouse-dot').stop()
+//     .css({ top : 10, left : 11, opacity : 1 })
+//     .delay(200)
+//   //   .animate({ top : 29, opacity : 0 },1500,'swing',function(){
+//   // $(this).css({ top : 10 })
+//   // .animate({ opacity : 1 },500,'swing',function(){ aniamate(); });
+// };
+// animate();
+// };
+
+(function($){
+	$.fn.percentPie = function(options){
+
+		var settings = $.extend({
+			width: 100,
+			trackColor: "F1C40F",
+			barColor: "777777",
+			barWeight: 30,
+			startPercent: 0,
+			endPercent: 1,
+			fps: 60
+		}, options);
+
+		this.css({
+			width: settings.width,
+			height: settings.width
+		});
+
+		var that = this,
+			hoverPolice = false,
+			canvasWidth = settings.width,
+			canvasHeight = canvasWidth,
+			id = $('canvas').length,
+			canvasElement = $('<canvas id="'+ id +'" width="' + canvasWidth + '" height="' + canvasHeight + '"></canvas>'),
+			canvas = canvasElement.get(0).getContext("2d"),
+			centerX = canvasWidth/2,
+			centerY = canvasHeight/2,
+			radius = settings.width/2 - settings.barWeight/2;
+			counterClockwise = false,
+			fps = 1000 / settings.fps,
+			update = .01;
+			this.angle = settings.startPercent;
+
+		this.drawArc = function(startAngle, percentFilled, color){
+			var drawingArc = true;
+			canvas.beginPath();
+			canvas.arc(centerX, centerY, radius, (Math.PI/180)*(startAngle * 360 - 90), (Math.PI/180)*(percentFilled * 360 - 90), counterClockwise);
+			canvas.strokeStyle = color;
+			canvas.lineWidth = settings.barWeight;
+			canvas.stroke();
+			drawingArc = false;
+		}
+
+		this.fillChart = function(stop){
+			var loop = setInterval(function(){
+				hoverPolice = true;
+				canvas.clearRect(0, 0, canvasWidth, canvasHeight);
+
+				that.drawArc(0, 360, settings.trackColor);
+				that.angle += update;
+				that.drawArc(settings.startPercent, that.angle, settings.barColor);
+
+				if(that.angle > stop){
+					clearInterval(loop);
+					hoverPolice = false;
+				}
+			}, fps);
+		}
+
+		this.mouseover(function(){
+			if(hoverPolice == false){
+				that.angle = settings.startPercent;
+				that.fillChart(settings.endPercent);
+			}
+		});
+
+		this.fillChart(settings.endPercent);
+		this.append(canvasElement);
+		return this;
+	}
+}(jQuery));
+
+$(document).ready(function() {
+
+	$('.statistic-chart-first').percentPie({
+		width: 170,
+		trackColor: "#5b5b5b",
+		barColor: "#f9b407",
+		barWeight: 20,
+		endPercent: .2,
+		fps: 30
+	});
+
+  $('.statistic-chart-second').percentPie({
+		width: 170,
+		trackColor: "#5b5b5b",
+		barColor: "#f7c20a",
+		barWeight: 20,
+		endPercent: .4,
+		fps: 30
+	});
+
+  $('.statistic-chart-third').percentPie({
+		width: 170,
+		trackColor: "#5b5b5b",
+		barColor: "#fbd414",
+		barWeight: 20,
+		endPercent: .5,
+		fps: 30
+	});
+
+  $('.statistic-chart-fourth').percentPie({
+		width: 170,
+		trackColor: "#5b5b5b",
+		barColor: "#f9db17",
+		barWeight: 20,
+		endPercent: .7,
+		fps: 30
+	});
+
+  $('.statistic-chart-fifth').percentPie({
+		width: 170,
+		trackColor: "#5b5b5b",
+		barColor: "#f7e524",
+		barWeight: 20,
+		endPercent: .8,
+		fps: 30
+	});
+
+  $('.statistic-chart-sixth').percentPie({
+		width: 170,
+		trackColor: "#5b5b5b",
+		barColor: "#f7e524",
+		barWeight: 20,
+		endPercent: .9,
+		fps: 60
+	});
+
 });
-animate();
-};
