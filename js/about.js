37b37b3231da02c04e667916202b3dbd22adfe1d
Evgeny Babkin <babkinevgeny@gmail.com>
2018-04-13 17:28:09 +0300

    new version of catalog

--- js/about.js
+++ js/about.js
@@ -1,3 +1,11 @@
+$('.dropdown-title .container').click(function() {
+  if($(this).parent().siblings('.container').children('.dropdown-item-content').hasClass('dropdown-item-content-show')) {
+    $(this).parent().siblings('.container').children('.dropdown-item-content').slideUp().removeClass('dropdown-item-content-show');
+  } else {
+    $(this).parent().siblings('.container').children('.dropdown-item-content').slideDown().addClass('dropdown-item-content-show');
+  }
+});
+
 $('.questions-title .container').click(function() {
   if($(this).parent().siblings('.container').children('.questions-item-content').hasClass('questions-item-content-show')) {
     $(this).parent().siblings('.container').children('.questions-item-content').slideUp().removeClass('questions-item-content-show');
